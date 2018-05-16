fcdb1b5c051ec7ff546f5a2def360a300f47c6f5
Daniel Dzhanlatian <dzhanlatyan@gmail.com>
2018-05-16 23:47:08 +0300

    Заменили  код

--- answers1.py
+++ answers1.py
@@ -1,23 +1,25 @@
-def get_answer(question, answers):
-    question = question.lower()
-    return answers.get(question, "Ошибка")
-
-
-
 answers = {
     "привет": "И тебе привет!",
     "как дела": "Лучше всех", 
     "пока": "Увидимся"
     }
 
-x = input("Привет. ")
-result = get_answer(x, answers)
-print(result)
 
-y = input("Как дела? ")
-result2 = get_answer(y, answers)
-print(result2)
+def get_answer(question, answer):
+	return answer.get(question)
 
-z = input("Пока ")
-result3 = get_answer(z, answers)
-print(result3)
+
+def ask_user(answers):
+	while True:
+		user_input = input("Скажи что-нибудь: ")
+		answer = get_answer(user_input, answers)
+		print(answer)
+
+		if user_input == "пока":
+			break
+
+
+if __name__=="__main__":
+	ask_user(answers)
+
+	
